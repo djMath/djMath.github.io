bc89852dd2d1cff6561b744797a02c28ec8daf8a
Jacob Samro <admin@jsam.in>
2015-07-02 23:31:59 +0530

    Autocomplete Working Module

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,15 @@
-$.getJSON( "database/formulas.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    console.log(key + ' => ' + val);
+var formulas = [];
+
+$.getJSON( "database/formulas.json", function( data ) {  
+  $.each( data, function( key, formula ) {
+    formulas.push({value : formula.title , data : key });
   });   
+});
+
+
+$('#ac').autocomplete({
+    lookup: formulas,
+    onSelect: function (suggestion) {
+        console.log("Formula : " + suggestion.value + "Mapped @ " + suggestion.data);
+    }
 });
