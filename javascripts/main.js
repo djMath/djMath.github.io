5e27c37528560d573c8f452c77a24a4d5bb768ca
Jacob Samro <admin@jsam.in>
2015-07-03 20:01:59 +0530

    formula Action Window click track

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@ var fieldLock = false; // denotes calculator state
 
 $.getJSON( "database/formulas.json", function( data ) {  
   $.each( data, function( key, formula ) {
-    formulas.push({value : formula.title , data : key });
+    formulas.push({value : formula.title , data : key , get : [ formula.expr_get ], out : [ formula.expr_out ]});
   });   
 });
 
@@ -12,6 +12,7 @@ $('.completeLoader').autocomplete({
     lookup: formulas,
     onSelect: function (suggestion) {
         console.log("Formula : " + suggestion.value + "Mapped @ " + suggestion.data);
+        load.formulaActionWindow(suggestion.value,suggestion.data);
     }
 });
 
@@ -25,10 +26,28 @@ var load = {
 		fieldLock = false;
 		console.log("Search Loaded");
 		$("#searchString").removeClass("calc");
+	},
+	formulaActionWindow : function(searchData,key){
+		$('.formulaActionWindow').fadeIn(150);
+		$('.formulaActionWindow h2').html(searchData);
+		var __form = formulas[key];
+		var length = __form.get.length;
+		console.log(length);
+		var radioHTML = "";
+		for (var i = 0; i < length; i++) {
+			radioHTML = "<input type='checkbox' name='getVal' value='" + __form.get[i] + "'>" + __form.get[i] + ""
+		};
+		$('#haveData').html(radioHTML);
 	}
 };
 
+//Action for Checkbox 
+$('body').on('click','#haveData input',function(){
+	console.log($(this));
+});
 
+
+//Search Initiator
 $("#searchString").keyup(function(){
 	var search = $(this).val();
 	if(fieldLock == false){
