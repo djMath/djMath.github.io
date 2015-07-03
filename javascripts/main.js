1bea93e9861f64bd19b667d16d71ff5fe7de4f45
Jacob Samro <admin@jsam.in>
2015-07-03 19:14:12 +0530

    Calculator and Search Switcher

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,5 @@
-var formulas = [];
+var formulas  = [];
+var fieldLock = false; // denotes calculator state
 
 $.getJSON( "database/formulas.json", function( data ) {  
   $.each( data, function( key, formula ) {
@@ -7,9 +8,40 @@ $.getJSON( "database/formulas.json", function( data ) {
 });
 
 
-$('#ac').autocomplete({
+$('.completeLoader').autocomplete({
     lookup: formulas,
     onSelect: function (suggestion) {
         console.log("Formula : " + suggestion.value + "Mapped @ " + suggestion.data);
     }
+});
+
+var load = {
+	calculator : function(){		
+		fieldLock = true;
+		$("#searchString").addClass("calc");
+		console.log("Calculator Loaded");
+	},
+	search : function(){
+		fieldLock = false;
+		console.log("Search Loaded");
+		$("#searchString").removeClass("calc");
+	}
+};
+
+
+$("#searchString").keyup(function(){
+	var search = $(this).val();
+	if(fieldLock == false){
+		if(!isNaN(search)&&search!=""){			
+			load.calculator();
+		}		
+	}else{
+		if(isNaN(search)){			
+			load.search();
+		}
+	}
+	if(search==""&&fieldLock == true){
+		load.search();
+	}
+
 });
